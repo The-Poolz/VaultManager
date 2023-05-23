a2509fc1d59082650570cf03a77d2a0a3254b970
Ashwin Arora <ashwinarora48@gmail.com>
2023-05-23 13:26:27 +0530

    compile fix

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 import "./IVault.sol";
 import "poolz-helper-v2/contracts/ERC20Helper.sol";
 
-contract Vault is IVault{
-    address public tokenAddress;
-    uint public tokenBalance;
+contract Vault is IVault, ERC20Helper{
+    address public override tokenAddress;
+    uint public override tokenBalance;
 
-    mapping(address => bool) public permittedDepositors;
-    mapping(address => bool) public permittedWithdrawers;
+    mapping(address => bool) public override permittedDepositors;
+    mapping(address => bool) public override permittedWithdrawers;
 
     constructor(address _tokenAddress){
         tokenAddress = _tokenAddress;
@@ -17,13 +17,13 @@ contract Vault is IVault{
         permittedWithdrawers[msg.sender] = true;
     }
 
-    function deposit(address from, uint _amount) external override {
+    function deposit(address from, uint _amount) external override { 
         require(permittedDepositors[msg.sender] == true, "Not permitted to deposit");
         TransferInToken(tokenAddress, from, _amount);
         tokenBalance += _amount;
     }
 
-    function withdraw(address to, uint _amount) external override{
+    function withdraw(address to, uint _amount) external override { 
         require(permittedWithdrawers[msg.sender] == true, "Not permitted to withdraw");
         require(tokenBalance >= _amount, "Not enough balance");
         TransferToken(tokenAddress, to, _amount);
