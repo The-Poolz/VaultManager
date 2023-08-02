6fa1684ffce7117682f638e8ba4d81541a2aabdb
Ashwin Arora <ashwinarora48@gmail.com>
2023-08-02 16:45:22 +0530

    typo fix

--- contracts/VaultManager/VaultRoyalty.sol
+++ contracts/VaultManager/VaultRoyalty.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
 contract VaultRoyalty is ERC2981, Ownable{
     event DefaultRoyaltySet(address indexed receiver, uint96 indexed feeNumerator);
-    event TokenRoyaltySet(uint indexed vaultId, address indexed receiver, uint96 indexed feeNumerator);
+    event VaultRoyaltySet(uint indexed vaultId, address indexed receiver, uint96 indexed feeNumerator);
 
     /// @dev used to set default royalty for all vaults
     /// @param receiver address of the royalty receiver
@@ -22,7 +22,7 @@ contract VaultRoyalty is ERC2981, Ownable{
     /// @dev used to set royalty for a specific vault (collection of LDNFTs)
     function setRoyaltyByVaultId(uint _vaultId, address receiver, uint96 feeNumerator) external onlyOwner{
         _setTokenRoyalty(_vaultId, receiver, feeNumerator);
-        emit TokenRoyaltySet(_vaultId, receiver, feeNumerator);
+        emit VaultRoyaltySet(_vaultId, receiver, feeNumerator);
     }
 
     function deleteDefaultRoyalty() external onlyOwner{
@@ -32,7 +32,7 @@ contract VaultRoyalty is ERC2981, Ownable{
 
     function resetVaultRoyalty(uint _vaultId) external onlyOwner{
         _resetTokenRoyalty(_vaultId);
-        emit TokenRoyaltySet(_vaultId, address(0), 0);
+        emit VaultRoyaltySet(_vaultId, address(0), 0);
     }    
 
 }
