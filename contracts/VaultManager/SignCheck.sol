2094e825d31d4ae5ca76a2a39e29d8d053b206af
Ashwin Arora <ashwinarora48@gmail.com>
2023-10-04 22:47:09 +0530

    minor fixes

--- contracts/VaultManager/SignCheck.sol
+++ contracts/VaultManager/SignCheck.sol
@@ -1,18 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 abstract contract SignCheck {
     using ECDSA for bytes32;
     mapping(address => uint) public nonces;
-
-    function prefixed(bytes32 hash) internal pure returns (bytes32) {
-        return
-            keccak256(
-                abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
-            );
-    }
 
     function _checkData(
         address from,
@@ -20,9 +13,7 @@ abstract contract SignCheck {
         bytes memory _signature
     ) internal returns (bool success) {
         uint currentNonce = nonces[from];
-        bytes32 hash = prefixed(
-            keccak256(abi.encodePacked(_data, currentNonce))
-        );
+        bytes32 hash = keccak256(abi.encodePacked(_data, currentNonce)).toEthSignedMessageHash();
         address signer = hash.recover(_signature);
 
         // Increment the nonce after verification
